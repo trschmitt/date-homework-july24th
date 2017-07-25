b04b0424addfc242060ef3e65c8a1feddd9f1528
Tristan Schmitt <trschmitt50@live.com>
2017-07-25 14:46:03 -0400

    defined all needed variables

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const chalk = require('chalk');
-var moment = require('moment');
-moment().format();
- 
+const moment = require('moment');
+
+let timeStamp = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+let day = moment().format("DDDo");
+let midnight = moment().startOf("day");
+let dayLightSave = moment().format("dddd, MMMM Do YYYY, h:mm:ss a").isDST();
+let leapYear = moment().format("YYYY").isLeapYear();
+
+let secFromMidnight = moment().diff(midnight, "seconds");
+
+
+console.log(`${chalk.blue(timeStamp)}`);
+console.log(`Today is the ${day} day of the year.`);
+console.log(`${chalk.secFromMidnight}`);
